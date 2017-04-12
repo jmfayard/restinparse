5d3ff23845d05cd1d5908c23d9556d28bbfb1637
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2017-04-12 12:09:30 +0200

    Reorganize project and publish it on Jitpack

--- restinparse-core/src/main/java/com/github/jmfayard/ParseColumn.java
+++
@@ -1,4 +0,0 @@
-package com.github.jmfayard;
-
-public interface ParseColumn {
-}
--- restinparse-core/src/main/java/com/github/jmfayard/internal/Strings.java
+++
@@ -1,9 +0,0 @@
-package com.github.jmfayard.internal;
-
-public class Strings {
-    public static boolean isNullOrEmpty(String order) {
-        return order == null || "".equals(order);
-    }
-
-
-}
